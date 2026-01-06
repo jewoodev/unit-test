59b6360227fc7562911b24d5bb97826267de1dce
jewoodev <jewoos15@naver.com>
2026-01-06 13:30:21 +0900

    init: 단위 테스트 학습 세팅

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
